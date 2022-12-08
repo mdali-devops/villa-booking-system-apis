53b807a3c5d3f64724ad2ad3eff5a0578f99d00e
mdali-devops <mdali.devops@gmail.com>
2022-12-09 00:16:35 +0500

    updated swagger docs

--- swagger.js
+++ swagger.js
@@ -1,6 +1,31 @@
-const swaggerAutogen = require('swagger-autogen')()
+// const swaggerAutogen = require('swagger-autogen')()
 
-const outputFile = './swagger_output.json'
-const endpointsFiles = ['./api/v1/routes/*.js','./api/v1/routes/bookings/*.js','./api/v1/routes/chats/*.js','./api/v1/routes/reports/*.js','./api/v1/routes/reviews/*.js','./api/v1/routes/supportTickets/*.js','./api/v1/routes/villas*.js']
+// const outputFile = './swagger_output.json'
+// const endpointsFiles = ['./api/v1/routes/*.js','./api/v1/routes/bookings/*.js','./api/v1/routes/chats/*.js','./api/v1/routes/reports/*.js','./api/v1/routes/reviews/*.js','./api/v1/routes/supportTickets/*.js','./api/v1/routes/villas*.js']
 
-swaggerAutogen(outputFile, endpointsFiles)
+// swaggerAutogen(outputFile, endpointsFiles)
+
+
+// const postmanToOpenApi = require('postman-to-openapi')
+
+// const postmanCollection = './Reviews and Ratings.postman_collection.json'
+// const outputFile = './reviewsandratings.json'
+
+// // Async/await
+// try {
+//     const result =  postmanToOpenApi(postmanCollection, outputFile, { defaultTag: 'General' })
+//     // Without save the result in a file
+//     const result2 = postmanToOpenApi(postmanCollection, null, { defaultTag: 'General' })
+//     console.log(`OpenAPI specs: ${result}`)
+// } catch (err) {
+//     console.log(err)
+// }
+
+// // Promise callback style
+// postmanToOpenApi(postmanCollection, outputFile, { defaultTag: 'General' })
+//     .then(result => {
+//         console.log(`OpenAPI specs: ${result}`)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
